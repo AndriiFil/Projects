4da35093faa3a8c1b7300d3d191be75cf17f8cd9
andrei <filimonov.andrei@gmail.com>
2019-11-30 00:32:19 +0200

    lesson 2 folder

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,24 +1,54 @@
 'use strict'
 
-var money;
-var time;
+let money = prompt("What is your month budget?",  '');
+let time = prompt("Введите дату в формате YYYY-MM-DD", '');
 
 var appData = {
-    budget: null,
-    timeData: "",
-    expenses: { resp1: 0, resp2:0 },
-    optionalExpenses: "",
-    income: null,
+    budget: money,
+    timeData: time,
+    expenses: { },
+    optionalExpenses: { },
+    income: [],
     savings: false
 };
 
-//prompt("What is your month budget?");
-appData.budget = prompt("What is your month budget?", 10000);
-appData.timeData = prompt("Введите дату в формате YYYY-MM-DD");
+for(let i = 0; i < 2; i++) {
+    let a = +prompt("Введите обязательную статью расходов в этом месяце");
+    let b = prompt("Во сколько обойдется?");
 
-appData.expenses.resp1 = prompt("Введите обязательную статью расходов в этом месяце");
-appData.expenses.resp2 = prompt("Во сколько обойдется?");
+    console.log(`a = ${a}, typeof(a): ` + typeof(a) + ", is null: " + (a === null));
+    console.log(`b = ${b}, typeof(b): ` + typeof(b) + ", is null: " + (b === null));
 
-alert(appData.budget / 30);
-console.log(appData.expenses.resp1 + "   " + appData.expenses.resp2);
+    if(a && b && a.length < 50) {
+        appData.expenses["val" + i] = b;
+    }
+}
 
+let dayBudget = appData.budget / 30;
+
+console.log(`Days budget is ${dayBudget} USD`);
+
+
+function exercise1() {
+    //prompt("What is your month budget?");
+    appData.budget = prompt("What is your month budget?", 10000);
+    //appData.timeData = prompt("Введите дату в формате YYYY-MM-DD");
+
+    appData.expenses.resp1 = prompt("Введите обязательную статью расходов в этом месяце");
+    appData.expenses.resp2 = prompt("Во сколько обойдется?");
+
+    let dayBudget = appData.budget / 30;
+
+    alert(`Days budget is ${dayBudget} USD`);
+    //console.log(appData.expenses.resp1 + "   " + appData.expenses.resp2);
+    console.log(`${appData.expenses.resp1}    ${appData.expenses.resp2}`);
+}
+
+function runTest() {
+    let y = [ ] + 1 + 2;
+    let tp = typeof(y);
+    console.log("-----------");
+    console.log(y);
+    console.log(`Type of y: ${tp}-----`);
+
+}
